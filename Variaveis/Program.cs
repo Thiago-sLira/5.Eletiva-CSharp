6cc55921394943e71937c6f4c7e9d06cf9ff1424
Thiago Lira <thiago.slira@outlook.com>
2023-07-03 10:48:59 -0300

    <feat>: Added instructions for Conditionals

--- Variaveis/Program.cs
+++ Variaveis/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// public class Program
+// {
+//   public static void Main()
+//   {
+//     // long someLongNumber = 516144066604654;
+//     string someString = "42";
+//     // int intNumber = (int) someLongNumber;
+//     // bool canConvert = Int32.TryParse(someString, out valueConverted)
+//     int intNumber = Convert.ToInt32(someString);
+
+//     Console.WriteLine(intNumber);
+//   }
+// }
