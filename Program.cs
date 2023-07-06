d1af78baa5a2d09ef08c2d4fa9033f66f7452840
Thiago Lira <thiago.slira@outlook.com>
2023-07-06 10:32:54 -0300

    Métodos para maniular strings

--- Program.cs
+++ Program.cs
@@ -5,3 +5,32 @@ string textThree = "e agora sabe concatenar textos utilizando a função Concat(
 string concatResult = string.Concat(textOne, textTwo, textThree);
 Console.WriteLine(concatResult);
 
+string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com";
+string[] arrayEmails = emails.Split(";");
+Console.WriteLine(arrayEmails[0]);
+
+string trybe = "I love Trybe";
+int index = trybe.IndexOf("y", 6);
+Console.WriteLine(index);
+
+List<string> languages = new List<string>
+{
+ "c#",
+ "java",
+ "javascript",
+ "python"
+};
+bool languageExists = languages.Contains("javac");
+
+if (languageExists)
+{
+  Console.WriteLine("Essa linguagem existe no sistema");
+}
+else
+{
+  Console.WriteLine("Essa linguagem não existe no sistema");
+}
+
+IEnumerable<int> listNumbers = Enumerable.Range(1, 10);
+string numbersText = string.Join(' ', listNumbers);
+Console.WriteLine(numbersText);
