84fdb19965b584f0962c93fa05906467d7d30624
Thiago Lira <thiago.slira@outlook.com>
2023-08-08 17:13:07 -0300

    Second commit

--- Content/Program.cs
+++ Content/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Content.BookContext;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        // builder.Services.AddDbContext<BookContext>();
+    }
+}
